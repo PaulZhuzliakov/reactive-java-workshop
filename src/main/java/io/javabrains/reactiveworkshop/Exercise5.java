b2866a1011d37bd503522c921ce80d5059eb9bcd
PaulZhuzliakov <paulparatrooper@gmail.com>
2022-11-10 04:37:33 +0700

    Exercise5

--- src/main/java/io/javabrains/reactiveworkshop/Exercise5.java
+++ src/main/java/io/javabrains/reactiveworkshop/Exercise5.java
@@ -1,21 +1,59 @@
 package io.javabrains.reactiveworkshop;
 
+import org.reactivestreams.Subscription;
+import reactor.core.publisher.BaseSubscriber;
+
 import java.io.IOException;
 
 public class Exercise5 {
 
     public static void main(String[] args) throws IOException {
 
-        // Use ReactiveSources.intNumberMono() and ReactiveSources.userMono()
+        // Use ReactiveSources.intNumbersFlux() and ReactiveSources.userFlux()
 
         // Subscribe to a flux using the error and completion hooks
-        // TODO: Write code here
+        ReactiveSources.intNumbersFlux().subscribe(
+                integer -> System.out.println(integer),
+                throwable -> System.out.println(throwable.getMessage()),
+                () -> System.out.println("end of intNumbersFlux")
+        );
+
+        ReactiveSources.userFlux().subscribe(
+                user -> System.out.println(user),
+                throwable -> System.out.println(throwable.getMessage()),
+                () -> System.out.println("end of userFlux")
+        );
 
         // Subscribe to a flux using an implementation of BaseSubscriber
-        // TODO: Write code here
+        ReactiveSources.intNumbersFlux().subscribe(
+                new MySubscriber<>()
+        );
+
+        ReactiveSources.userFlux().subscribe(
+                new MySubscriber<>()
+        );
 
         System.out.println("Press a key to end");
         System.in.read();
     }
 
+}
+
+class MySubscriber<T> extends BaseSubscriber<T> {
+    @Override
+    protected void hookOnSubscribe(Subscription subscription) {
+        System.out.println("subscription happend");
+        request(2);
+    }
+
+    @Override
+    protected void hookOnNext(T value) {
+        System.out.println(value.toString() + " reseived");
+        request(2);
+    }
+
+    @Override
+    protected void hookOnComplete() {
+        System.out.println("completed");
+    }
 }
