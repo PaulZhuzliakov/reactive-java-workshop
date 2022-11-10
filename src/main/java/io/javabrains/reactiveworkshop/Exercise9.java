75cf6bda4c9dc2c05a2d34be299bad52a14c659a
PaulZhuzliakov <paulparatrooper@gmail.com>
2022-11-10 08:06:30 +0700

    Exercises 9

--- src/main/java/io/javabrains/reactiveworkshop/Exercise9.java
+++ src/main/java/io/javabrains/reactiveworkshop/Exercise9.java
@@ -3,20 +3,38 @@ package io.javabrains.reactiveworkshop;
 import java.io.IOException;
 
 public class Exercise9 {
-
+    private static final int BUFFER_SIZE = 2;
 
     public static void main(String[] args) throws IOException {
+
 
         // Use ReactiveSources.intNumbersFlux()
 
         // Print size of intNumbersFlux after the last item returns
-        // TODO: Write code here
+//        ReactiveSources.intNumbersFlux()
+//                .count()
+//                .subscribe(aLong -> System.out.println(aLong));
 
         // Collect all items of intNumbersFlux into a single list and print it
-        // TODO: Write code here
+//        ReactiveSources.intNumbersFlux()
+//                .collectList()
+//                .subscribe(i -> System.out.println(i));
 
         // Transform to a sequence of sums of adjacent two numbers
-        // TODO: Write code here
+
+        ReactiveSources.intNumbersFlux()
+                .buffer(BUFFER_SIZE)
+                .map(integers -> {
+                    int int1 = integers.get(0);
+                    int int2;
+                    if (integers.size() == BUFFER_SIZE) {
+                        int2 = integers.get(1);
+                    } else {
+                        int2 = 0;
+                    }
+                    return int1 + int2;
+                })
+                .subscribe(integer -> System.out.println(integer));
 
         System.out.println("Press a key to end");
         System.in.read();
